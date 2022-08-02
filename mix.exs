6fea2ce0ccbec8f739e6e677775cbfd0ff678baa
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-02 17:03:52 +0900

    install and set up ex_doc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule BTriangle.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "BTriangle",
+      docs: [
+        main: "BTriangle",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -23,6 +28,7 @@ defmodule BTriangle.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
   end
 end
