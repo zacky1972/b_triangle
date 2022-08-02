7207a145f9b531da8fcc3d19d21b92654cf28406
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-02 18:06:05 +0900

    implement read_graph

--- lib/b_triangle.ex
+++ lib/b_triangle.ex
@@ -2,4 +2,48 @@ defmodule BTriangle do
   @moduledoc """
   Documentation for `BTriangle`.
   """
+
+  @type graph() :: %{pos_integer() => %{pos_integer() => pos_integer()}}
+
+  @spec read_graph(String.t()) :: graph()
+  def read_graph(str) do
+    line =
+      str
+      |> String.split("\n")
+      |> Enum.reject(&(&1 == ""))
+
+    [nm | uv] = line
+
+    [n, m] =
+      nm
+      |> String.split(" ")
+      |> Enum.map(&String.to_integer/1)
+
+    uv =
+      Enum.take(uv, m)
+      |> Enum.map(
+        &(&1
+          |> String.split(" ")
+          |> Enum.map(fn x -> String.to_integer(x) end)
+          |> List.to_tuple())
+      )
+
+    Enum.reduce(uv, graph_init(n), fn {u, v}, map -> graph_add(map, u, v) end)
+  end
+
+  @spec graph_init(pos_integer()) :: graph()
+  def graph_init(n) do
+    1..n
+    |> Enum.map(fn _ -> %{} end)
+    |> Enum.with_index()
+    |> Enum.map(fn {v, k} -> {k + 1, v} end)
+    |> Map.new()
+  end
+
+  @spec graph_add(graph(), pos_integer(), pos_integer()) :: graph()
+  def graph_add(map, u, v) do
+    map
+    |> Map.put(u, Map.get(map, u) |> Map.put(v, v))
+    |> Map.put(v, Map.get(map, v) |> Map.put(u, u))
+  end
 end
--- test/b_triangle_test.exs
+++ test/b_triangle_test.exs
@@ -1,4 +1,33 @@
 defmodule BTriangleTest do
   use ExUnit.Case
   doctest BTriangle
+
+  test "read_graph input1" do
+    assert BTriangle.read_graph(File.read!("test/support/input1.txt")) ==
+             %{
+               1 => %{4 => 4, 5 => 5},
+               2 => %{3 => 3, 5 => 5},
+               3 => %{2 => 2, 5 => 5},
+               4 => %{1 => 1, 5 => 5},
+               5 => %{1 => 1, 2 => 2, 3 => 3, 4 => 4}
+             }
+  end
+
+  test "read_graph input2" do
+    assert BTriangle.read_graph(File.read!("test/support/input2.txt")) ==
+             %{1 => %{2 => 2}, 2 => %{1 => 1}, 3 => %{}}
+  end
+
+  test "read_graph input3" do
+    assert BTriangle.read_graph(File.read!("test/support/input3.txt")) ==
+             %{
+               1 => %{3 => 3, 5 => 5, 6 => 6, 7 => 7},
+               2 => %{4 => 4, 5 => 5, 7 => 7},
+               3 => %{1 => 1, 6 => 6},
+               4 => %{2 => 2, 7 => 7},
+               5 => %{1 => 1, 2 => 2, 7 => 7},
+               6 => %{1 => 1, 3 => 3},
+               7 => %{1 => 1, 2 => 2, 4 => 4, 5 => 5}
+             }
+  end
 end
