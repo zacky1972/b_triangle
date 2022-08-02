5c80e9521d2d4bc9b716686d86a9781a407acc46
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-02 18:11:54 +0900

    add num of node of graph

--- lib/b_triangle.ex
+++ lib/b_triangle.ex
@@ -5,7 +5,7 @@ defmodule BTriangle do
 
   @type graph() :: %{pos_integer() => %{pos_integer() => pos_integer()}}
 
-  @spec read_graph(String.t()) :: graph()
+  @spec read_graph(String.t()) :: {pos_integer(), graph()}
   def read_graph(str) do
     line =
       str
@@ -28,7 +28,7 @@ defmodule BTriangle do
           |> List.to_tuple())
       )
 
-    Enum.reduce(uv, graph_init(n), fn {u, v}, map -> graph_add(map, u, v) end)
+    {n, Enum.reduce(uv, graph_init(n), fn {u, v}, map -> graph_add(map, u, v) end)}
   end
 
   @spec graph_init(pos_integer()) :: graph()
