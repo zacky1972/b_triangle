28ea58bd156f0a7771825d1598a074cddfc15932
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-02 17:00:50 +0900

    remove hello

--- lib/b_triangle.ex
+++ lib/b_triangle.ex
@@ -2,17 +2,4 @@ defmodule BTriangle do
   @moduledoc """
   Documentation for `BTriangle`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BTriangle.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/b_triangle_test.exs
+++ test/b_triangle_test.exs
@@ -1,8 +1,4 @@
 defmodule BTriangleTest do
   use ExUnit.Case
   doctest BTriangle
-
-  test "greets the world" do
-    assert BTriangle.hello() == :world
-  end
 end
