d812de57db53c428479d035583d8a09170901b73
Susumu Yamazaki <zacky1972@gmail.com>
2022-08-02 16:59:44 +0900

    initial commit

---
+++ lib/b_triangle.ex
@@ -0,0 +1,18 @@
+defmodule BTriangle do
+  @moduledoc """
+  Documentation for `BTriangle`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BTriangle.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/b_triangle_test.exs
@@ -0,0 +1,8 @@
+defmodule BTriangleTest do
+  use ExUnit.Case
+  doctest BTriangle
+
+  test "greets the world" do
+    assert BTriangle.hello() == :world
+  end
+end
